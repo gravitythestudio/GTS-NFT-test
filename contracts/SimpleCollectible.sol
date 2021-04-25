9fecb729cb505cd6c44caacdb8277ca1bf85f3a8
gravitythestudio <82963254+gravitythestudio@users.noreply.github.com>
2021-04-25 15:59:22 +0100

    Update SimpleCollectible.sol

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -11,11 +11,10 @@ contract SimpleCollectible is ERC721 {
         tokenCounter = 0;
     }
 
-    function createCollectible(string memory) public returns (uint256) {
+    function createCollectible(string memory tokenURI) public returns (uint256) {
         uint256 newItemId = tokenCounter;
         _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
         tokenCounter = tokenCounter + 1;
         return newItemId;
     }
-
-}
